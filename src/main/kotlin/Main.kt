5795156c43e6c8aac4e13b8816f481ec86613104
Nishant Kumar Singh <52786028+nishu9162306344@users.noreply.github.com>
2022-07-29 00:07:36 +0530

    Add all my files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,56 @@
+
+fun main(){
+
+    println("exection starting.....")
+
+//    //variable
+//    var x =5
+//    x=9
+//    // var x : Int =5;
+//
+//    var y=10
+//
+//    var z=x*y
+//
+//    var name ="Nishant Kumar Singh"
+//  println("The value of x is  $name")
+//
+//
+//   var p=11
+//   var q=11
+//
+//    if(p==q){
+//        println("Good..........")
+//
+//    }else{
+//
+//        print("Not Good...........")
+//    }
+//
+//    var p=readLine() ?:"0"
+//    var q=readLine() ?:"0"
+//
+//
+//
+//    println(p.toInt()+q.toInt())
+//    try{
+//       println("Good..........")
+//    }catch(e: Exception){
+//print(e.message)
+//    }
+//    var list =listOf("Nishant","subahb");
+//    println(list)
+
+
+    // mutableListOf------> it is mutable help us to addd tyhe string
+//
+//    add()
+}
+//
+//fun add(){
+//
+//    var x=10
+//    var y=12
+//    var z=x+y
+//    print(x+y)
+}
